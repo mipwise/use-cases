073c4ba7d51af844f966505426a22b3d53f78d00
Aster Santana <50426897+astersantana@users.noreply.github.com>
2022-09-29 16:00:40 -0300

    Pastesian: Added status check for the optimization output

--- pastesian/scripts/pastesian_capacity_pulp.py
+++ pastesian/scripts/pastesian_capacity_pulp.py
@@ -24,10 +24,15 @@ mdl.setObjective(5.50*x[1] + 7.20*x[2] + 8.80*x[3] + 10.90*x[4] + 1.30*s[1] + 1.
                  + 0.35*(y[1] + y[2] + y[3] + y[4]))
 
 # Optimize
-mdl.solve()
+status_code = mdl.solve()
 
 # Retrieve the solution
-x_sol = {t: x[t].value() for t in [1, 2, 3, 4]}
-s_sol = {t: s[t].value() for t in [1, 2, 3]}
-print(f'Amount to produce = {x_sol}')
-print(f'Amount to store = {s_sol}')
+status = pulp.LpStatus[status_code]
+if status == 'Optimal':
+    print(f'Optimal solution found!')
+    x_sol = {t: x[t].value() for t in [1, 2, 3, 4]}
+    s_sol = {t: s[t].value() for t in [1, 2, 3]}
+    print(f'Amount to produce = {x_sol}')
+    print(f'Amount to store = {s_sol}')
+else:
+    print(f'Model is not optimal. Status: {status}')
